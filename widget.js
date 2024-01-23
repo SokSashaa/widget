9b2ad0f4977dfb0c55554ef2185b931f2387191d
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-01-24 01:36:34 +0300

    addDatePicker

---
+++ widget.js
@@ -0,0 +1,13 @@
+const depDateBut = new Datepicker('#depDate',{
+    toValue: (date)=>{
+        const input = document.querySelector('#forDepDate');
+        input.value = date
+    }
+});
+
+const retDateBut = new Datepicker('#retDate',{
+    toValue: (date)=>{
+        const input = document.querySelector('#forRetDate');
+        input.value = date
+    }
+});
