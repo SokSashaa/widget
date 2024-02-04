0aee68c3b26d4d3fc67873213816935adc37a7e6
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-02-05 01:02:05 +0300

    finishWidgetJS

--- exportWidget.js
+++ exportWidget.js
@@ -1,15 +1,58 @@
-import {rootW} from "./widget/exp.js";
+const rootElementWidget = () => {
+    return `
+    <h1>Where does it come from? Why do we use it?</h1>
+    <form>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking
+            at its layout</p>
+        <div class="inputWithIcon"><input type="text" placeholder="Depart date" id="forDepDate">
+            <button type="button" id="depDate"></button>
+        </div>
+        <div class="inputWithIcon"><input type="text" placeholder="Return date" id="forRetDate">
+            <button type="button" id="retDate"></button>
+        </div>
+        <button class="formButton">Search</button>
+    </form>`
+}
+
+const checkSize = (size) =>{
+    return size>'299px';
+};
 
 window.onload = () => {
     const root = document.querySelector('#rootWidget');
-    const rootWidth = root.getAttribute('data-width');
-    const frame = document.createElement('iframe');
-    frame.setAttribute('width', rootWidth);
-    frame.setAttribute('height','200px');
-    frame.setAttribute('src', './widget/widget.html');
-    root.insertAdjacentElement('beforeend', frame);
 
-    //console.log(rootW);
+    const elWidget = document.createElement('div');
+    elWidget.id = 'mainWidget';
+
+    if (root.hasAttribute('data-width') && checkSize(root.getAttribute('data-width'))) {
+        elWidget.style.width = root.getAttribute('data-width');
+    } else {
+        elWidget.style.width = '1024px';
+    }
+
+    elWidget.innerHTML = rootElementWidget();
+
+
+
+    root.insertAdjacentElement('beforeend', elWidget);
+
+    const depDateBut = new Datepicker('#depDate', {
+        toValue: (date) => {
+            const input = document.querySelector('#forDepDate');
+            input.value = date
+        }
+    });
+
+    const retDateBut = new Datepicker('#retDate', {
+        toValue: (date) => {
+            const input = document.querySelector('#forRetDate');
+            input.value = date
+        }
+    });
 };
+
+
+
+
 
 
--- widget/widget.js
+++ widget/widget.js
@@ -1,16 +1,2 @@
-const depDateBut = new Datepicker('#depDate',{
-    toValue: (date)=>{
-        const input = document.querySelector('#forDepDate');
-        input.value = date
-    }
-});
-
-const retDateBut = new Datepicker('#retDate',{
-    toValue: (date)=>{
-        const input = document.querySelector('#forRetDate');
-        input.value = date
-    }
-});
-
 
 
