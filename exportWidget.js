8b8e3b3dbeaa381d45e861d7593b6f375d5f19e1
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-02-15 22:25:28 +0300

    lastVersion

--- exportWidget.js
+++ exportWidget.js
@@ -1,7 +1,7 @@
-const rootElementWidget = () => {
+const rootElementWidget = (classForm) => {
     return `
     <h1>Where does it come from? Why do we use it?</h1>
-    <form>
+    <form class="${classForm}">
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking
             at its layout</p>
         <div class="inputWithIcon"><input type="text" placeholder="Depart date" id="forDepDate">
@@ -14,24 +14,31 @@ const rootElementWidget = () => {
     </form>`
 }
 
-const checkSize = (size) =>{
-    return size>'299px';
+const checkSize = (size) => {
+    return size > 299 && size < 1025;
 };
+const switchClassWidget = (size) => {
+    if (size > 299 && size < 701) return 'M700'
+    else return 'M900'
+}
 
 window.onload = () => {
     const root = document.querySelector('#rootWidget');
 
     const elWidget = document.createElement('div');
     elWidget.id = 'mainWidget';
+    let classForm;
 
     if (root.hasAttribute('data-width') && checkSize(root.getAttribute('data-width'))) {
-        elWidget.style.width = root.getAttribute('data-width');
+        const gAttribute = root.getAttribute('data-width');
+        elWidget.style.width = gAttribute + 'px';
+        classForm = switchClassWidget(gAttribute);
     } else {
         elWidget.style.width = '1024px';
+        classForm = 'formWidget'
     }
 
-    elWidget.innerHTML = rootElementWidget();
-
+    elWidget.innerHTML = rootElementWidget(classForm);
 
 
     root.insertAdjacentElement('beforeend', elWidget);
